528e80f5c9a51c178b8f11431f2df685b4be3935
SpicyGengu <lovro.antic1@gmail.com>
2025-02-13 16:59:13 +0100

    now they I can connect strings

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,13 @@ fn write_to_file(path: &str, contents: &str) -> Result<(), Box<dyn std::error::E
 fn main() {
     let file_name: &str = "logs.txt";
     let mut log_content: &str = "now look at this";
+    let mut log_content_addon: &str = "\nstrong or smtn";
+    match write_to_file(&file_name, &log_content) {
+        Ok(_) => (),
+        Err(_) => println!("couldn't write to file")
+    }
+    let binding = (log_content.to_owned() + log_content_addon);
+    log_content = binding.as_str();
     match write_to_file(&file_name, &log_content) {
         Ok(_) => (),
         Err(_) => println!("couldn't write to file")
